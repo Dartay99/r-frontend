6b4e8ecb58a5621ac3ae3f3881f47a6e370e4f86
Dartay Azh <dartay99@gmail.com>
2023-05-29 00:27:22 +0600

    new_lesson

---
+++ controller/product.js
@@ -0,0 +1,20 @@
+import axios from "axios"
+
+const productsController = {
+  async getProducts() {
+    return await axios.get("http://localhost:3000/products");
+  },
+  async getProduct(id) {
+    return await axios.get(`http://localhost:3000/products/${id}`);
+  },
+  async createProduct(product) {
+    return await axios.post("http://localhost:3000/products",product);
+  },
+  async updateProducts(id, product) {
+    return await axios.put(`http://localhost:3000/products/${id}`,product);
+  },
+  async deleteProducts(id) {
+    return await axios.delete(`http://localhost:3000/products/${id}`);
+  }
+}
+export default productsController
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,19 +1,33 @@
-import Button from "../components/Button"
 import Navbar from "../components/Navbar"
+import productsController from "../../controller/product"
+import { useState } from "react";
 
 const Products = () => {
+  const[products,setProducts]= useState([]);
+  const[loading,setLoading]=useState(false)
 
-const sayHello=()=>{
-  alert("Hello")
-}
-  
+
+  const fetchProducts = async ()=>{
+    setLoading(true);
+    const{data, status} = await productsController.getProducts();
+    alert(status);
+    console.log(data);
+    setProducts(data);
+    setLoading(false);
+  };
+
   return(
     <div>
       < Navbar />
       <h1>Products</h1>
-      <Button text="Add to Cart" onClick={sayHello}/>
+      <button onClick={fetchProducts}>fetch products</button>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) :(
+        products.map((product)=> <h1 key={product.id}>{product.title}<br/>{product.price}</h1> )
+      )}
     </div>
-  )
+  );
 }
 
 export default Products
